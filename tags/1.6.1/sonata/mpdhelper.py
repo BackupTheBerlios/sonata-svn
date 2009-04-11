13b5f2a600336feff8c07fb36d3cc062c98f66ec
stonecrest <stonecrest@d0cb7a4c-441d-0410-9d93-c5902a6597d6>
2009-04-11 19:22:30 +0000

    release trunk revision 1192 as sonata 1.6.1

---
+++ tags/1.6.1/sonata/mpdhelper.py
@@ -0,0 +1,109 @@
+
+import locale, sys, os
+from time import strftime
+from misc import remove_list_duplicates
+
+suppress_errors = False
+
+def suppress_mpd_errors(val):
+	global suppress_errors
+	suppress_errors = val
+
+def status(client):
+	result = call(client, 'status')
+	if result and 'state' in result:
+		return result
+	else:
+		return {}
+
+def currsong(client):
+	return call(client, 'currentsong')
+
+def get(mapping, key, alt=''):
+	# Returns either the value in the dict or, currently, the
+	# first list's values. e.g. this will return 'foo' if genres
+	# is ['foo' 'bar']. This should always be used to retrieve
+	# values from a mpd song.
+	value = mapping.get(key, alt)
+	if isinstance(value, list):
+		return value[0]
+	else:
+		return value
+	
+def getnum(mapping, key, alt='0', return_int=False, str_padding=0):
+	# Same as get(), but sanitizes the number before returning
+	tag = get(mapping, key, alt)
+	return sanitize(tag, return_int, str_padding)
+
+def sanitize(tag, return_int, str_padding):
+	# Sanitizes a mpd tag; used for numerical tags. Known forms 
+	# for the mpd tag can be "4", "4/10", and "4,10".
+	ret = 0
+
+	split_tag = tag.replace(',',' ',1).replace('/',' ',1).split()
+	if split_tag[0].isdigit():
+		ret = int(split_tag[0])
+
+	return ret if return_int else str(ret).zfill(str_padding)
+	
+def conout(s):
+	# A kind of 'print' which does not throw exceptions if the string 
+	# to print cannot be converted to console encoding; instead it 
+	# does a "readable" conversion
+	print s.encode(locale.getpreferredencoding(), "replace")
+
+def call(mpdclient, mpd_cmd, *mpd_args):
+	try:
+		retval = getattr(mpdclient, mpd_cmd)(*mpd_args)
+	except:
+		if not mpd_cmd in ['disconnect', 'lsinfo', 'listplaylists']:
+			if not suppress_errors:
+				print strftime("%Y-%m-%d %H:%M:%S") + "  " + str(sys.exc_info()[1])
+		if mpd_cmd in ['lsinfo', 'list']:
+			return []
+		else:
+			return None
+
+	return retval
+
+def mpd_major_version(client):
+	try:
+		version = getattr(client, "mpd_version", 0.0)
+		parts = version.split(".")
+		return float(parts[0] + "." + parts[1])
+	except:
+		return 0.0
+
+def mpd_is_updating(status):
+	return status and status.get('updating_db', 0)
+
+def update(mpdclient, paths, status):
+	# mpd 0.14.x limits the number of paths that can be
+	# updated within a command_list at 32. If we have
+	# >32 directories, we bail and update the entire library.
+	#
+	# If we want to get trickier in the future, we can find 
+	# the 32 most specific parents that cover the set of files.
+	# This would lower the possibility of resorting to a full
+	# library update.
+	#
+	# Note: If a future version of mpd relaxes this limit,
+	# we should make the version check more specific to 0.14.x
+	
+	if mpd_is_updating(status):
+		return
+	
+	# Updating paths seems to be faster than updating files for
+	# some reason:
+	dirs = []
+	for path in paths:
+		dirs.append(os.path.dirname(path))
+	dirs = remove_list_duplicates(dirs, True)
+
+	if len(dirs) > 32 and mpd_major_version(mpdclient) >= 0.14:
+		call(mpdclient, 'update', '/')
+	else:
+		call(mpdclient, 'command_list_ok_begin')
+		for directory in dirs:
+			call(mpdclient, 'update', directory)
+		call(mpdclient, 'command_list_end')
---
+++ tags/1.6.1/sonata/plugins/test.py
@@ -0,0 +1,56 @@
+
+# this is the magic interpreted by Sonata, referring to on_enable etc. below:
+
+### BEGIN PLUGIN INFO
+# [plugin]
+# plugin_format: 0, 0
+# name: Test plugin
+# version: 0, 0, 0
+# description: A simple test plugin.
+# author: Tuukka Hastrup
+# author_email: Tuukka.Hastrup@iki.fi
+# url: http://sonata.berlios.de
+# [capabilities]
+# enablables: on_enable
+# tabs: construct_tab
+# playing_song_observers: on_song_change
+### END PLUGIN INFO
+
+# nothing magical from here on
+
+import gtk, pango
+
+from sonata.misc import escape_html
+
+songlabel = None
+
+# this gets called when the plugin is loaded, enabled, or disabled:
+def on_enable(state):
+	global songlabel
+	if state:
+		songlabel = gtk.Label("No song info received yet.")
+		songlabel.props.ellipsize = pango.ELLIPSIZE_END
+	else:
+		songlabel = None
+
+# this constructs the parts of the tab when called:
+def construct_tab():
+	vbox = gtk.VBox()
+	vbox.pack_start(gtk.Label("Hello world!"))
+	vbox.pack_start(songlabel)
+	vbox.pack_start(gtk.Label("(You can modify me at %s)" % 
+				  __file__.rstrip("c")))
+	vbox.show_all()
+
+	# the return value goes off to Base.new_tab(page, stock, text, focus):
+	# (tab content, icon name, tab name, the widget to focus on tab switch)
+	return (vbox, None, "Test plugin", None)
+
+# this gets called when a new song is playing:
+def on_song_change(songinfo):
+	if songinfo:
+		songlabel.set_markup("<b>Info for currently playing song:</b>"+
+				     "\n%s" % escape_html(repr(songinfo)))
+	else:
+		songlabel.set_text("Currently not playing any song.")
+	songlabel.show()
